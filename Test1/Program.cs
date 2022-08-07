48636c46eddfa011239fe8f5d18ddf77bb3336b1
litovkomilada <litovkomiada@yandex.ru>
2022-08-07 18:33:17 +0300

    test commit

---
+++ Test1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите длину массива");
+int number = Convert.ToInt32(Console.ReadLine());
+string[] arrayOfString = new string[number]; 
+
+for(int i = 0; i < number; i++)
+{
+    Console.WriteLine($"Введите строку номер {i+1}");
+    string currentString = Console.ReadLine();
+    arrayOfString[i] = currentString;
+}
+Console.WriteLine(string.Join(",",arrayOfString));
